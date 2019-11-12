a05dc7960e43cfb838d8d8afe7c12daeaff42cd2
kylebertram <kyle.bertram@students.olin.edu>
2019-11-11 23:54:56 -0500

    Experiment with audio and surround sound

--- runningQuestions.md
+++ runningQuestions.md
@@ -1,2 +1,4 @@
 1. How do we create a inclusive experience for users in both audio and visual input?
 2. How to use stereo audio?
+	- Google Resonance
+	- Open AL
--- soundExploration.py
+++ soundExploration.py
@@ -1,5 +1,8 @@
+"""
 import pygame
 from time import sleep
+import random
+#import sndlib
 
 # intialise mixer
 pygame.mixer.init()
@@ -9,6 +12,20 @@ ping = pygame.mixer.Sound("Sounds/ping_2.wav")
 foot = pygame.mixer.Sound("Sounds/footstep_cement.wav")
 hollow = pygame.mixer.Sound("Sounds/drop.wav")
 
+stone_step = []
+for i in range(1,7):
+    stone_step.append(pygame.mixer.Sound("Sounds/stoneSteps/stone"+str(i)+".ogg"))
+"""
+"""
+i = 0
+while i < 6:
+    #step = random.choice(stone_step)
+    step = stone_step[i]
+    step.play()
+    sleep(.2)
+    i+=1
+"""
+'''
 # Play the file and put it into a channel object
 channel = ping.play()
 #channel = foot.play()
@@ -34,5 +51,17 @@ channel = ping.play()
 channel.set_volume(1,1)
 sleep(3)
 channel = ping.play()
-channel.set_volume(.8,.05)
+channel.set_volume(.8,0)
+sleep(.1)
+channel = ping.play()
+channel.set_volume(0,.4)
 sleep(3)
+'''
+
+
+
+from gtts import gTTS
+import os
+tts = gTTS(text='Hi my name is Kyle Bertram', lang='en-uk')
+tts.save("good.mp3")
+os.system("mpg321 good.mp3")
